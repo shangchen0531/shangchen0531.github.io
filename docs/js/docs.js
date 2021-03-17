8a15812f52028184c89bf40b52c26e689acdcfde
Shangchen <shangchen0531@gmail.com>
2021-03-17 08:40:32 +0800

    搬运Nelson Rodrigues的卡牌特效

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -84,16 +84,37 @@
 
 (function(){
     // 集换式卡牌特效
-    const card = document.querySelector('.steam__card')
-
-    card.addEventListener('mousemove', event => {
-    const y = event.offsetY - 180,
-            xRotation = y / -360,
-            x = event.offsetX - 180,
-            yRotation = x / 360,
-            brightness = (Math.abs(-360 + x + y) / 360) + .5;
-    card.style.setProperty('--x-rotation', xRotation + 'rad');
-    card.style.setProperty('--y-rotation', yRotation + 'rad');
-    card.style.setProperty('--brightness', brightness);
+    function map(val, minA, maxA, minB, maxB) {
+        return minB + ((val - minA) * (maxB - minB)) / (maxA - minA);
+    }
+      
+    function Card3D(card, ev) {
+        let img = card.querySelector('img');
+        let imgRect = card.getBoundingClientRect();
+        let width = imgRect.width;
+        let height = imgRect.height;
+        let mouseX = ev.offsetX;
+        let mouseY = ev.offsetY;
+        let rotateY = map(mouseX, 0, 180, -25, 25);
+        let rotateX = map(mouseY, 0, 250, 25, -25);
+        let brightness = map(mouseY, 0, 250, 1.5, 0.5);
+        
+        img.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        img.style.filter = `brightness(${brightness})`;
+    }
+    
+    var cards = document.querySelectorAll('.card3d');
+    
+    cards.forEach((card) => {
+        card.addEventListener('mousemove', (ev) => {
+            Card3D(card, ev);
+        });
+    
+        card.addEventListener('mouseleave', (ev) => {
+            let img = card.querySelector('img');
+        
+            img.style.transform = 'rotateX(0deg) rotateY(0deg)';
+            img.style.filter = 'brightness(1)';
+        });
     });
 }());
